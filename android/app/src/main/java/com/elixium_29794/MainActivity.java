552c3413aa2a8fb3fa42340c8628eac6b28580bc
Crowdbotics <team@crowdbotics.com>
2021-08-13 21:30:18 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/elixium_29794/MainActivity.java
@@ -0,0 +1,15 @@
+package com.elixium_29794;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "elixium_29794";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "elixium_29794Identifier",
+  url: "https://elixium_29794Identifier.botics.co",
+  api: "https://elixium_29794Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
